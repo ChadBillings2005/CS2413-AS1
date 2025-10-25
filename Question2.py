5ad6d13c28b630802174b5173d1d5db18e438194
candiceinmanunb <o5fr9@unb.ca>
2025-10-25 18:56:59 -0300

    add Question2 file and keys-cipher file provided by professor.

---
+++ Question2.py
@@ -0,0 +1,18 @@
+from Crypto.Hash import SHA256
+from Crypto.Hash import MD5
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES 
+from Crypto.PublicKey import RSA 
+from Crypto.Cipher import PKCS1_OAEP 
+
+
+
+
+
+
+
+
+
+
+
