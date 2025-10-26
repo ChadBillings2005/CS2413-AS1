c2e2dbd5b97d5b3d100da46aa786fb384c150a2d
candiceinmanunb <o5fr9@unb.ca>
2025-10-25 21:35:16 -0300

    update code for question 3.

--- Question3.py
+++ Question3.py
@@ -1,17 +1,19 @@
-from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA 
 from Crypto.Cipher import PKCS1_OAEP 
 import base64
 
 with open("private_key.pem", "rb") as f:
-		private_key = f.read()
+		private_key = RSA.import_key(f.read())
 
-#print(private_key[:50])
+with open("encrypted_message.txt", "r") as f:
+		encrypted_data = base64.b64decode(f.read().strip())
 
-sha256_hash = SHA256.new(private_key).hexdigest()
-print("\nSHA-256 hash from file: ", sha256_hash, "\n")
+print("\nEncrypted cipher text loaded from file:\n", encrypted_data)
 
+rsa_cipher = PKCS1_OAEP.new(private_key)
 
+decrypted_data = rsa_cipher.decrypt(encrypted_data)
 
+print("\nDecrypted message: \n", decrypted_data.decode(), "\n")
 
 
